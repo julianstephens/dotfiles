8d97148e2d00b657343cab81b0cb31aa6c0a4717
Julian Stephens <julianstephens55@gmail.com>
2021-06-04 20:39:57 -0400

    Logout backup

--- .my_commands.sh
+++ .my_commands.sh
@@ -4,12 +4,12 @@ function create() {
     cd
     mkdir /mnt/d/workspace/$1
     cd /mnt/d/workspace/$1
-    git init
+    git init -b main
     git remote add origin git@github.com:julianstephens/$1
     touch README.md
     git add .
     git commit -m "Initial commit"
-    git push -u origin master
+    git push -u origin main
 }
 
 # source ~/.my_commands.sh
